feaaae32f81568d7e6d5776c840b090bd06df157
Fernando Ribeiro <fernando.ribeiro357@gmail.com>
2024-09-21 02:17:27 -0300

    verificar chute no 'Enter', reiniciar jogo no 'Esc'

--- src/app.js
+++ src/app.js
@@ -6,25 +6,28 @@ let tentativas = 1;
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
-    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.2});
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', { rate: 1.2 });
 }
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
     exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    limparCampo();
 }
 
 exibirMensagemInicial();
 
+
 function verificarChute() {
     let chute = document.querySelector('input').value;
-    
+
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou!');
         let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
         let mensagemTentativas = `Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`;
         exibirTextoNaTela('p', mensagemTentativas);
         document.getElementById('reiniciar').removeAttribute('disabled');
+
     } else {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', `O número secreto é menor que ${chute}`);
@@ -53,9 +56,9 @@ function gerarNumeroAleatorio() {
 }
 
 function limparCampo() {
-    chute = document.querySelector('input');
-    chute.value = '';
-    chute.focus();
+    inputChute = document.querySelector('input');
+    inputChute.value = '';
+    inputChute.focus();
 }
 
 function reiniciarJogo() {
@@ -66,9 +69,16 @@ function reiniciarJogo() {
     document.getElementById('reiniciar').setAttribute('disabled', true)
 }
 
-
-
-
-
-
-
+document.querySelector('input').addEventListener('keydown', (event) => {
+        switch (event.key) {
+            case 'Enter':
+                verificarChute();
+                break;
+            
+            case 'Escape':
+                // reiniciarJogo();
+                document.getElementById('reiniciar').click();
+                break;
+        }
+    }, true,
+);
